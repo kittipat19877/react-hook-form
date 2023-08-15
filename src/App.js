0faf5187971f5d7f8d20b95f35ffee75cb1c04ec
kittipat19877 <number8671@gmail.com>
2023-08-15 14:57:49 +0700

    create react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react';
+import { Button, Container,Form } from 'react-bootstrap';
+import {useForm} from "react-hook-form"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const {register, handleSubmit} = useForm();
+  const [data, setData] = useState();
+  return ( 
+    <Container>
+      <h4>React Hook Form</h4>
+      <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <Form.Group className='mb-3'>
+          <Form.Label>Email Adress</Form.Label>
+          <Form.Control {...register("email")} type="email" placeholder="Enter Email" />
+          </Form.Group>
+          <Form.Group className='mb-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control {...register("password")}type="password" placeholder="Enter Password" />
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Label>ประเภทสมาชิก</Form.Label>
+          <Form.Select {...register("usertype")}>
+            <option value="1" selected>User</option>
+            <option value="2">VIP</option>
+            <option value="3">Admin</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className='mb-3'>
+          <Form.Check {...register("remember")}type="checkbox" label="Remember Me" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Text>{data}</Form.Text>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
